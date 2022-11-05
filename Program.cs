8e5224153c98631fc57d1b58b1729ccab06edbc4
weslley.souza@soupromove.com.br <weslley.souza@soupromove.com.br>
2022-11-04 22:12:54 -0300

    Finalizado desafio DIO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia!");
+Smartphone nokia = new Nokia("332233", "NK025", "7908414408210", 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
+Console.WriteLine($"Modelo: {nokia.Modelo}");
+
+
+Console.WriteLine("----------------------------------------------------------------------------");
+
+Console.WriteLine("Ipone!");
+Smartphone iphone = new Iphone("222200", "MPVX3BE/A", "194253488279", 256);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("WhatsApp");
+Console.WriteLine($"Modelo: {iphone.Modelo}");
